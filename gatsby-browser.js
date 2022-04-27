94f6ef95ef41d75d2a946765c645e32ae3b88f5e
Kelly Knights <kknights@katalystadvantage.com>
2022-04-26 21:48:21 -0400

    admin | added scss

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,6 +5,9 @@ import "typeface-merriweather"
 import "./src/normalize.css"
 // custom CSS styles
 import "./src/style.css"
+// SCSS
+import "./src/styles.scss"
+
 
 // Highlighting for code blocks
 import "prismjs/themes/prism.css"
